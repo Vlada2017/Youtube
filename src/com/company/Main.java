5a279d35a32a2a1193745f884ee6f2f5dac64b47
Vlada2017 <sunbakshi19@gmail.com>
2017-03-13 15:26:10 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.err.println("Hello!");
+        int a;
+        a = 5;
+
+        if (a == 5 ) {
+            for (int i = 0; i < 2; i++)
+                System.out.print(" . ");
+        }
+        if (a == 5) {
+            System.out.print(" * ");
+        }
+        if (a == 5) {
+            for (int i = 0; i < 2; i++)
+                System.out.print(" . ");
+        }
+        System.out.println();
+        if (a == 5) {
+            for (int i = 0; i < 1; i++)
+                System.out.print(" . ");
+        }
+        if (a == 5) {
+            System.out.print(" * ");
+        }
+        if (a == 5) {
+            for (int i = 0; i < 1; i++)
+                System.out.print(" . ");
+        }
+        if (a == 5) {
+            System.out.print(" * ");
+        }
+        if (a == 5) {
+            for (int i = 0; i < 1; i++)
+                System.out.print(" . ");
+        }
+        System.out.println();
+        if (a == 5) {
+            for (int i = 0; i < 2; i++)
+                System.out.print(" . ");
+        }
+        if (a == 5) {
+            System.out.print(" * ");
+        }
+        if (a == 5) {
+            for (int i = 0; i < 2; i++)
+                System.out.print(" . ");
+        }
+    }
+}
